5ce40f9889f48530e6b489402a6dc1d01a8b1047
zsy <djytyang@gmail.com>
2018-04-04 15:42:57 +0800

    Add project files.

---
+++ lab1_1/lab1_1.cpp
@@ -0,0 +1,37 @@
+//
+// Created by zhu on 2018/3/3.
+//
+
+#include <iostream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+
+using namespace std;
+using namespace cv;
+
+void bgr2rgb(Mat &input, Mat &output);
+
+int main(int argc, char **argv) {
+
+	Mat input_img = imread(argv[1]);
+	Mat output_img = input_img.clone();
+
+	bgr2rgb(input_img, output_img);
+	imshow("origin", input_img);
+	imshow("bgr2rgb", output_img);
+	waitKey(0);
+
+	imwrite("output.jpg", output_img);
+
+	return 0;
+}
+
+void bgr2rgb(Mat &input, Mat &output) {
+	for (int i = 0; i < input.rows; i++) {
+		for (int j = 0; j < input.cols; j++) {
+			output.at<Vec3b>(i, j)[0] = input.at<Vec3b>(i, j)[2];
+			output.at<Vec3b>(i, j)[2] = input.at<Vec3b>(i, j)[0];
+		}
+	}
+}
